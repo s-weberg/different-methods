9e933213013f46c450dae64b76370f8859ee753a
s-weberg <sandraweberg90@gmail.com>
2025-03-24 17:13:48 +0100

    updates js file

--- index.js
+++ index.js
@@ -9,32 +9,26 @@ console.log(originalPrice);
 //2. Convert the input from string to number 
 
 
-let numberFirst = "49.99";
-let number = parseFloat(numberFirst)
-console.log(number);
-
-
- 
-/*
-//3. Reduce the price by 10 procent
-let price = 50;
-let sale = 10;
-let result = (sale * price) / (100);
-let newPrice = (price - result); */
 
 
 let priceWithoutDollar = originalPrice.slice(1);
 console.log(priceWithoutDollar);
+
+
+
+let number = parseFloat(priceWithoutDollar);
+console.log(number);
+
+
 
 //4. Print out the new price using console.log(), for example: 'Your new price is: $44.99' 
 
 newPrice = (priceWithoutDollar * 0.9);
 console.log(newPrice);
 
-console.log(`The new price for this dress is $${newPrice}`)
+let salePrice = 44.991;
+let sP = salePrice.toFixed(1);
 
-/*
-console.log(price - result);
-console.log(`The new price for this dress is $${newPrice}`)
-*/
- 
+console.log(`The new price for this dress is $${sP}`)
+
+
